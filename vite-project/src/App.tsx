b198527da1746c74a66217ec2b77e64c736123c6
AishwaryaKaranth <aishwaryakaranth27899@gmail.com>
2024-02-01 22:22:33 +0530

    chore: file parse WIP

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
+import OpenAI from 'openai'
+import pdfParse from 'pdf-parse'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [file, setFile]=useState(null);
+  const handleFileUpload=(event: any)=>{
+    setFile(event.target.files[0])
+  }
+
+  const processFile=()=>{
+    if(file){
+      const reader = new FileReader();
+      reader.readAsText(file, 'UTF-8')
+      reader.onloadend=()=>{
+        const data = reader.result
+        parseFile(data)
+      }
+    }
+  }
+
+  const parseFile=(data: string | ArrayBuffer | null)=>{
+    const parsedText = pdfParse(data)
+    console.log(parsedText)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Listen Now!</h1>
+      <input type="file" accept=".pdf" onChange={handleFileUpload}/>
+      <button onClick={processFile}>Upload</button>
     </>
   )
 }
